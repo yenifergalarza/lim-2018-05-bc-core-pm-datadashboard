28a4935316465ede6a15c90d2b43fc12f1a3180c
yenifergalarza <yeniferalexandra2000@hotmail.com>
2018-06-15 16:34:27 -0500

    cambios

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const fileUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const infoAlumnas = document.querySelector('#listadoAlumnas');
+
+fetch(fileUsers).then(function(response) {
+   return response.json();
+})
+.then(json => {
+   let alumnas = json;
+   for(let i = 0; i < alumnas.length; i++){
+       if(alumnas[i].role == 'student'){
+           const element = document.createElement('p'); //crea un <p>
+           const contenidoElement = document.createTextNode(alumnas[i].name); //
+
+           element.appendChild(contenidoElement);
+           infoAlumnas.appendChild(element);
+       }  
+   }
+});
+
+const selectElement = document.getElementById('sedes');
+
+
+fetch('../data/cohorts.json')
+.then(response => response.json())
+.then(json => {
+   const sedes = json;
+   console.log(sedes.length);
+   for(let i = 0; i < sedes.length; i ++) {
+       const optionElements = document.createElement('option');
+       const contenidoOption = document.createTextNode(sedes[i].id);
+       optionElements.appendChild(contenidoOption);
+       console.log(sedes[i].id);
+       selectElement.appendChild(optionElements);
+   }
+})
+.catch((err) => {
+  // algo salió mal...
+  console.error(err);
+});
